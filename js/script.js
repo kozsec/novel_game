4788ff29ab6d769cc448b803bd3629429475ef05
koz <tik.kozawa@gmail.com>
2026-05-25 00:11:01 +0900

    background

--- js/script.js
+++ js/script.js
@@ -1,44 +1,39 @@
 let scenario = [];
-
 let index = 0;
 
 const nameElement = document.getElementById("name");
-
 const textElement = document.getElementById("text");
+const bgElement =  document.getElementById("background");
 
-const nextBtn = document.getElementById("next-btn");
-
+/*================
+シナリオ読み込み
+================*/
 fetch("./data/scenario.json")
   .then(response => response.json())
   .then(data => {
-
     scenario = data;
-
     showMessage();
-
   });
 
+/*================
+次のメッセージを表示
+================*/
 function showMessage() {
-
   const message = scenario[index];
-
   nameElement.textContent = message.name;
-
   textElement.textContent = message.text;
-
+  if (message.bg) { /*背景画像があれば上書き*/
+    backgroundElement.src = message.bg;
+  }
 }
 
-
+/*================
+クリックしたら(画面全体)
+================*/
 document.body.addEventListener("click", () => {
-
   index++;
-
   if (index >= scenario.length) {
-
     index = 0;
-
   }
-
   showMessage();
-
 });
